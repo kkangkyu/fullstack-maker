f0fcb36319194342cf91d3f29ddd49c4937a3372
kkangkyu <48673297+kkangkyu@users.noreply.github.com>
2025-11-19 16:08:53 +0900

    Layout component

--- app/potato/about.tsx
+++ app/potato/about.tsx
@@ -3,12 +3,18 @@ export default function AboutUs(){
         <div>
             <h1>About Us</h1>
             <h1>Our misstion</h1>
-            <p>We are a team of developers who are passionate about creating software that is easy to use and understand.</p>
-            <h2>We are a team of developers who are passionate about creating software that is easy to use and understand.</h2>
         </div>
-
-
-
-
     )
 }
+
+export const links = () => [
+    {
+        rel: "stylesheet",
+        href: "https://naver.com",
+    },
+];
+
+export const meta = () => [
+    {
+        title: "wemake" }
+];
