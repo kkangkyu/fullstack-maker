2ba98704aa7020323e2744c064cef8b0fb96e5cb
kkangkyu <48673297+kkangkyu@users.noreply.github.com>
2025-11-19 17:36:11 +0900

    basics-shadcn

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,5 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-    index("routes/home.tsx"),
-    route("/about", "potato/about.tsx")
+    index("routes/home.tsx")
 ] satisfies RouteConfig;
