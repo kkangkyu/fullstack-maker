25c60a5cc1777da7d89c28706cb1ca0d454c5147
kkangkyu <48673297+kkangkyu@users.noreply.github.com>
2025-11-19 15:22:21 +0900

    first commit

--- app/potato/about.tsx
+++ app/potato/about.tsx
@@ -3,6 +3,7 @@ export default function AboutUs(){
         <div>
             <h1>About Us</h1>
             <h1>Our misstion</h1>
+            <p>We are a team of developers who are passionate about creating software that is easy to use and understand.</p>
             <h2>We are a team of developers who are passionate about creating software that is easy to use and understand.</h2>
         </div>
 
